ef46fe1769129b2461a5e664d30c913951a8ac41
Kobayashi <43286862+kh1r0@users.noreply.github.com>
2018-09-27 01:53:35 +0900

    温度判定処理(プロトタイプ)

--- hkoba/JudgeTemperature.py
+++ hkoba/JudgeTemperature.py
@@ -1,1 +1,19 @@
+#-*- coding: utf-8 -*-
+import pandas as pd
+import time
 
+dateparse = lambda x: pd.datetime.strptime(x, '%Y/%m/%d %H:%M:%S')
+df = pd.read_csv('test.csv', header=None, names=['datetime', 'deviceID', 'temperature'], parse_dates=['datetime'], date_parser=dateparse)
+
+#for i, r in df.iterrows():
+#    print type(r['datetime']), type(r['deviceID']), type(r['temperature'])
+
+df_s = df.sort_index(axis=0, ascending=False)
+
+#print(df_s)
+#print(df_s.head(1))
+#print(df_s.iloc[0])
+print("Temperature:%6.2f" % df_s['temperature'].values[0])
+
+if (df_s['temperature'].values[0] >= 20):
+  print("over limit")
